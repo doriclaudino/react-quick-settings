c4026584dc82068a91bff6699554a9ae27401021
dori claudino <dori.claudino@gmail.com>
2020-02-13 00:50:52 +0000

    slider examples

--- src/components/QuickPanel.js
+++ src/components/QuickPanel.js
@@ -17,14 +17,14 @@ export class QuickSettingsPanel extends Component {
     }
   
     /** should we add an type? */
-    addSlider = ({title='slider01', options}) => {
+    addSlider = ({title='slider01', ...rest}) => {
       this.setState({
         controls: {
           ...this.state.controls,
           [title]: {
             inputType:  inputType.slider,
             title,
-            options
+            ...rest
           }
         }
       });
@@ -62,11 +62,11 @@ export class QuickSettingsPanel extends Component {
         <div>
           {Object.keys(this.state.controls).map((mappedKeyItem) => {
               let item = this.state.controls[mappedKeyItem]
-              const {title, options, inputType} = item              
+              const {title, inputType, ...rest} = item              
               let Component = this.getComponentForType(inputType)
               let key = this.generateNewKey(item)
               console.log(title)
-              return <Component key id={key} {...options}/>  
+              return <Component key={key} id={key} {...rest}/>  
           })
           }
         </div>
--- src/index.js
+++ src/index.js
@@ -6,5 +6,29 @@ import { QuickSettings } from "./components/QuickSettings";
 
 window.QuickSettings = QuickSettings;
 
+/**
+ * quick example using slides https://material-ui.com/pt/components/slider/#api
+ */
+const marks = [
+    {
+      value: 0,
+      label: '0°C',
+    },
+    {
+      value: 20,
+      label: '20°C',
+    },
+    {
+      value: 37,
+      label: '37°C',
+    },
+    {
+      value: 100,
+      label: '100°C',
+    },
+  ];
 let q = new QuickSettings(document.getElementById('container'));
-q.addSlider({title:'how_many_cats',value:25, onChange:(v)=>console.log(v)})
+q.addSlider({title:'how_many_cats1', defaultValue:25, marks, onChange:(obj, value)=>console.log(value)})
+q.addSlider({title:'how_many_cats2', defaultValue:25, onChange:(obj, value)=>console.log(value)})
+q.addSlider({title:'how_many_cats3', defaultValue:25, onChange:(obj, value)=>console.log(value)})
+q.addSlider({title:'how_many_cats4', defaultValue:25, onChange:(obj, value)=>console.log(value)})
