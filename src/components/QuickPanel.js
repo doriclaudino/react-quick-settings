d39c1b98d27372cf00f3bd8276c66d83ec619afb
dori claudino <dori.claudino@gmail.com>
2020-02-13 01:46:38 +0000

    color pickers

--- src/components/QuickPanel.js
+++ src/components/QuickPanel.js
@@ -1,75 +1,96 @@
 import { Slider as _SliderComponent } from '@material-ui/core';
-import React,  { Component }  from "react";
+import React, { Component } from "react";
+import * as reactColorPicker from 'react-color'
 
-var inputType  = {
-    slider: 'slider'
-  };
+var inputType = {
+  slider: 'slider',
+  colorPicker: 'colorPicker',
+};
 
 /**
  * Main class
  */
 export class QuickSettingsPanel extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        controls: {}
-      };
-    }
-  
-    /** should we add an type? */
-    addSlider = ({title='slider01', ...rest}) => {
-      this.setState({
-        controls: {
-          ...this.state.controls,
-          [title]: {
-            inputType:  inputType.slider,
-            title,
-            ...rest
-          }
-        }
-      });
-      return this;
+  constructor(props) {
+    super(props);
+    this.state = {
+      controls: {}
     };
-  
-    /** remove control object using title as index
-     * what about same title? generate ids?
-     */
-    remove = title => {
-      let controls = { ...this.state.controls };
-      delete controls[title];
-      this.setState({
-        controls
-      });
-      return this;
-    };
+  }
 
-    getComponentForType = (type)=>{
-        switch (type) {
-            case inputType.slider:
-                return _SliderComponent;
-            default:
-                break;
+  /** should we add an type? */
+  addSlider = ({ title = `${inputType.slider}01`, ...rest }) => {
+    this.setState({
+      controls: {
+        ...this.state.controls,
+        [title]: {
+          inputType: inputType.slider,
+          componentName: undefined,
+          title,
+          ...rest
         }
-    }
+      }
+    });
+    return this;
+  };
 
-    generateNewKey = (control) => `control_${control.inputType}_${control.title}` 
-  
-    /**
-     * for now just render the state.controls as JSON
-     */
-    render() {
-      return (
-        <div>
-          {Object.keys(this.state.controls).map((mappedKeyItem) => {
-              let item = this.state.controls[mappedKeyItem]
-              const {title, inputType, ...rest} = item              
-              let Component = this.getComponentForType(inputType)
-              let key = this.generateNewKey(item)
-              console.log(title)
-              return <Component key={key} id={key} {...rest}/>  
-          })
-          }
-        </div>
-      );
+  /** should we add an type? */
+  addColorPicker = ({ title = `${inputType.colorPicker}01`, componentName = 'SketchPicker', ...rest }) => {
+    this.setState({
+      controls: {
+        ...this.state.controls,
+        [title]: {
+          inputType: inputType.colorPicker,
+          componentName,
+          title,
+          ...rest
+        }
+      }
+    });
+    return this;
+  };
+
+  /** remove control object using title as index
+   * what about same title? generate ids?
+   */
+  remove = title => {
+    let controls = { ...this.state.controls };
+    delete controls[title];
+    this.setState({
+      controls
+    });
+    return this;
+  };
+
+  getComponentForType = (type, componentName) => {
+    switch (type) {
+      case inputType.slider:
+        return _SliderComponent;
+      case inputType.colorPicker:
+        return reactColorPicker[componentName];
+      default:
+        break;
     }
   }
+
+  generateNewKey = (control) => `control_${control.inputType}_${control.title}`
+
+  /**
+   * for now just render the state.controls as JSON
+   */
+  render() {
+    return (
+      <div>
+        {Object.keys(this.state.controls).map((mappedKeyItem) => {
+          let item = this.state.controls[mappedKeyItem]
+          const { title, inputType, componentName, ...rest } = item
+          let Component = this.getComponentForType(inputType, componentName)
+          let key = this.generateNewKey(item)
+          console.log(title)
+          return <Component  {...rest} key={key} id={key}/>
+        })
+        }
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -11,24 +11,42 @@ window.QuickSettings = QuickSettings;
  */
 const marks = [
     {
-      value: 0,
-      label: '0°C',
+        value: 0,
+        label: '0°C',
     },
     {
-      value: 20,
-      label: '20°C',
+        value: 20,
+        label: '20°C',
     },
     {
-      value: 37,
-      label: '37°C',
+        value: 37,
+        label: '37°C',
     },
     {
-      value: 100,
-      label: '100°C',
+        value: 100,
+        label: '100°C',
     },
-  ];
+];
 let q = new QuickSettings(document.getElementById('container'));
-q.addSlider({title:'how_many_cats1', defaultValue:25, marks, onChange:(obj, value)=>console.log(value)})
-q.addSlider({title:'how_many_cats2', defaultValue:25, onChange:(obj, value)=>console.log(value)})
-q.addSlider({title:'how_many_cats3', defaultValue:25, onChange:(obj, value)=>console.log(value)})
-q.addSlider({title:'how_many_cats4', defaultValue:25, onChange:(obj, value)=>console.log(value)})
+q.addSlider({ title: 'how_many_cats1', defaultValue: 25, marks, onChange: (obj, value) => console.log(value) })
+q.addSlider({ title: 'how_many_cats2', defaultValue: 25, onChange: (obj, value) => console.log(value) })
+q.addSlider({ title: 'how_many_cats3', defaultValue: 25, onChange: (obj, value) => console.log(value) })
+q.addSlider({ title: 'how_many_cats4', defaultValue: 25, onChange: (obj, value) => console.log(value) })
+
+
+let color = '#7b1fa2'
+/** example with color pickers */
+q.addColorPicker({ title: 'car_color_01',  onChange:(color,event)=>{console.log(color);}})
+q.addColorPicker({ title: 'car_color_02', color, componentName:'SwatchesPicker', onChange:(ecolor,event)=>{
+    let c = {...q.state.controls['car_color_02']}
+    c.color=ecolor.hex 
+    q.setState({
+        controls: {
+            ...q.state.controls,
+            ['car_color_02']: c
+        }
+    })
+   // q.state.controls['car_color_02'].color=ecolor.hex 
+}})
+q.addColorPicker({ title: 'car_color_03', componentName:'CompactPicker', onChange:(ecolor,event)=>console.log(ecolor) })
+console.log(q)
