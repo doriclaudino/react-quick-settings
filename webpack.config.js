60dc202037efe91e3651fddf04175ef0519092f4
dori claudino <dori.claudino@gmail.com>
2020-02-21 19:12:54 -0500

    packing and linking the module to local test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+// const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  devtool: 'source-map',
+  // devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'main.js',
+    library: 'ReactQuickSettings',
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true,
+  },
   module: {
     rules: [
       {
@@ -11,20 +20,42 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
-      },
+      // {
+      //   test: /\.html$/,
+      //   use: [
+      //     {
+      //       loader: 'html-loader',
+      //     },
+      //   ],
+      // },
     ],
   },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-  ],
+  // plugins: [
+  //   new HtmlWebPackPlugin({
+  //     template: './src/index.html',
+  //     filename: './index.html',
+  //   }),
+  // ],
+
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM',
+    },
+  },
 };
